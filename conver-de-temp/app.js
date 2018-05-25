a489d725e7b8f10de3b4bf7f490173eabba969ed
annaoble <31705873+annaoble@users.noreply.github.com>
2018-05-25 08:38:21 -0500

    Este es mi primer repositorio
    repositorio reto laboratoria.

---
+++ conver-de-temp/app.js
@@ -0,0 +1,6 @@
+//preguntar la temp en celsius
+var temperature = parseInt(prompt("¿Cual es la temperatura en Celsius?"));
+//convertir celsius a Fahrenheit
+var convertToFahrenheit = (temperature*1.8) + 32;
+//mostrar resultado en consola
+console.log("La temperatura en Fahrenheit es: " + convertToFahrenheit);
